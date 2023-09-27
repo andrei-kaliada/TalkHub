2419b50244ba97dd7253531f68b40647e68a4bec
andrei-kaliada <koleda05@gmail.com>
2023-09-27 16:39:13 +0200

    Initialized default structure.

---
+++ front/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'TalkHub',
+  description: "TalkHub is a cutting-edge chat and communication platform designed to bring people closer, whether they're across the street or across the globe.",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
